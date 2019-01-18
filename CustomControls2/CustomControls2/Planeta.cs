0cea6585f2ab87093cc1e25b434c6e0eae6d781e
kevinmurgi <kevinmurgi@gmail.com>
2019-01-18 10:22:24 +0100

    cambios

---
+++ CustomControls2/CustomControls2/Planeta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace CustomControls2
+{
+    public class Planeta
+    {
+        public string Nombre { get; set; }
+        public double Diametro { get; set; }
+        public double DistanciaSol { get; set; }
+        public SolidColorBrush Color { get; set; }
+        public string Imagen { get; set; }
+    }
+
+    public class GestorPlanetas
+    {
+        public static List<Planeta> ObtenerPlanetas()
+        {
+            return new List<Planeta>
+            {
+                //new Planeta() {Nombre = "Mercurio", Diametro = 5, DistanciaSol = 100, Color = new SolidColorBrush(Colors.Gray)},
+                //new Planeta() {Nombre = "Venus", Diametro = 20, DistanciaSol = 150, Color = new SolidColorBrush(Colors.Yellow)},
+                //new Planeta() {Nombre = "Tierra", Diametro = 15, DistanciaSol = 200, Color = new SolidColorBrush(Colors.Blue)},
+                //new Planeta() {Nombre = "Marte", Diametro = 10, DistanciaSol = 250, Color = new SolidColorBrush(Colors.Red)},
+                new Planeta() {Nombre = "Jupiter", Diametro = 90, DistanciaSol = 350, Imagen =  "Assets/jupiter.jpg", Color = new SolidColorBrush(Colors.Brown)},
+                new Planeta() {Nombre = "Saturno", Diametro = 80, DistanciaSol = 450, Color = new SolidColorBrush(Colors.Brown), Imagen =  "Assets/saturno.jpg"},
+                //new Planeta() {Nombre = "Urano", Diametro = 45, DistanciaSol = 550, Color = new SolidColorBrush(Colors.Cyan)},
+                //new Planeta() {Nombre = "Neptuno", Diametro = 45, DistanciaSol = 650, Color = new SolidColorBrush(Colors.Blue)}
+                
+            };
+        }
+    }
+}
---
+++ CustomControls2/CustomControls2/SolarSystem.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Shapes;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace CustomControls2
+{
+    public sealed class SolarSystem : Control
+    {
+        // Atributos
+        Canvas _canvas;
+        private int radioSol = 75;
+
+        // Constructor
+        public SolarSystem()
+        {
+            this.DefaultStyleKey = typeof(SolarSystem);
+        }
+
+        /*
+         * Metodo OnApplyTemplate, para pintar dentro del Canvas
+         */
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _canvas = (Canvas)GetTemplateChild("canvasTemplate");
+            if (_canvas != null)
+            {
+                crearPlanetas();
+            }
+        }
+
+        private void crearPlanetas()
+        {
+            if (ItemsSource != null)
+            {
+                _canvas.Children.Clear();
+                foreach (var item in ItemsSource)
+                {
+                    var element = new Ellipse();
+                    //element.Fill = item.Color;
+                    element.Fill = new ImageBrush() { ImageSource = new BitmapImage() { UriSource = new Uri("ms-appx:///"+item.Imagen) } };
+                    element.Width = element.Height = item.Diametro;
+                    _canvas.Children.Add(element);
+                    Canvas.SetLeft(element, (radioSol * -1) + (item.DistanciaSol * -1));
+                    Canvas.SetTop(element, (item.Diametro/2) * -1);
+                }
+            }
+        }
+
+        /*
+         * ItemsSource
+         */
+        public List<Planeta> ItemsSource
+        {
+            get { return (List<Planeta>)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register("ItemsSource", typeof(List<Planeta>), typeof(SolarSystem), new PropertyMetadata(null));
+
+        /*
+         * Max Item Size
+         */
+        public double MaxItemSize
+        {
+            get { return (double)GetValue(MaxItemSizeProperty); }
+            set { SetValue(MaxItemSizeProperty, value); }
+        }
+        public static readonly DependencyProperty MaxItemSizeProperty =
+            DependencyProperty.Register("MaxItemSize", typeof(double), typeof(SolarSystem), new PropertyMetadata(null));
+
+        /*
+         * Min Item Size
+         */
+        public double MinItemSize
+        {
+            get { return (double)GetValue(MinItemSizeProperty); }
+            set { SetValue(MinItemSizeProperty, value); }
+        }
+        public static readonly DependencyProperty MinItemSizeProperty =
+            DependencyProperty.Register("MinItemSize", typeof(double), typeof(SolarSystem), new PropertyMetadata(null));
+    }
+}
