6e533dbbaecd6904f9f6ac9c35c3d64f7af5de7b
kevinmurgi <kevinmurgi@gmail.com>
2019-03-05 08:17:26 +0100

    nuevo proyecto

---
+++ JsonThings/JsonThings/Data/JsonData.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using JsonThings.Models;
+
+namespace JsonThings.Data
+{
+    public class JsonData
+    {
+        /**
+         * Metodo que devuelve el post enviado como parametro.
+         */
+        public async static Task<Post> GetPost(int post)
+        {
+            // Intancia de un objeto HttpClient
+            HttpClient http = new HttpClient();
+
+            // Recuperamos los datos que nos devuelve la API
+            var respuesta = await http.GetAsync("http://localhost:3000/posts/" + post);
+
+            // Recogemos ese resultado como un String
+            var resultado = await respuesta.Content.ReadAsStringAsync();
+
+            // Instanciamos un serializador que convierta los bytes en un objeto ConversionEuros
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Post));
+
+            // Creamos un stream al que le pasamos el resultado de la consulta en bytes
+            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(resultado));
+
+            // Generamos el objeto ConverisonEuros utilizando el serializador
+            Post datos = (Post)serializer.ReadObject(ms);
+
+            // Devolvemos el objeto obtenido
+            return datos;
+        }
+
+        /**
+         * Metodo que devuelve todos los posts.
+         */
+        public async static Task<List<Post>> GetPosts()
+        {
+            // Intancia de un objeto HttpClient
+            HttpClient http = new HttpClient();
+
+            // Recuperamos los datos que nos devuelve la API
+            var respuesta = await http.GetAsync("http://localhost:3000/posts");
+
+            // Recogemos ese resultado como un String
+            var resultado = await respuesta.Content.ReadAsStringAsync();
+
+            // Instanciamos un serializador que convierta los bytes en un objeto ConversionEuros
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(List<Post>));
+
+            // Creamos un stream al que le pasamos el resultado de la consulta en bytes
+            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(resultado));
+
+            // Generamos el objeto ConverisonEuros utilizando el serializador
+            List<Post> datos = (List<Post>)serializer.ReadObject(ms);
+
+            // Devolvemos el objeto obtenido
+            return datos;
+        }
+    }
+}
---
+++ JsonThings/JsonThings/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonThings.Models
+{
+    public class Comment
+    {
+        public int id { get; set; }
+        public string body { get; set; }
+        public int postId { get; set; }
+    }
+}
